e8b64c2f078cf8a465eda909e6382a81c5e10395
nasimabo <nasimuddin555k@gmail.com>
2024-02-22 23:07:38 +0600

    Arithmetic operator python

---
+++ arithmetic_operator.py
@@ -0,0 +1,65 @@
+################# Arithmetic operator ####################
+
+# sum
+'''num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+sum = num1 + num2
+print(sum)'''
+
+# sub
+
+num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+
+sub = num1 - num2
+print(sub)
+
+# mul
+
+num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+
+mul = num1 * num2
+print(mul)
+
+# div
+
+num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+
+div = num1 / num2
+print(div)
+
+# mode
+
+num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+
+mode = num1 % num2
+print(mode)
+
+
+# squre
+
+num1 = int(input("enter the num 1 : "))
+num2 = int(input("enter the num 2 : "))
+
+squre = num1 ** num2
+print(squre)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
